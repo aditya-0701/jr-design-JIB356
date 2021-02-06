f45f715118d58bc6ad2fac2ad5f03d8877efb0e4
Aditya Sudarshan <aditya.sudzz@gmail.com>
2021-02-06 00:03:50 -0500

    Implented the controller and model for the Student table

--- matchingapp-api/index.js
+++ matchingapp-api/index.js
@@ -13,10 +13,11 @@
 // });
 
 // connection.end();
+const connection = require('./src/dbInit');
 
-var stud = require('./src/models/studentModels');
+const Student = require('./src/models/studentModels');
 
-var stu = new stud.Student({
+var stu = new Student({
     'gtUsername': 'randomUser',
     'firstName': 'random',
     'lastName': 'user',
@@ -26,6 +27,14 @@ var stu = new stud.Student({
 
 //return stud.Student.getAll({filter: `WHERE gtUsername = "asudarshan30" `});
 //stud.Student.addStudent(stu);
-stud.Student.getAll({filter: null});
+async function test () {
+    //await Student.updateStudent({gtUsername: 'randomUser', firstName: 'New', lastName: 'NewLast'});
+    console.log(await Student.findStudent({gtUsername: 'randomUser'}))
+    connection.end();
+}
+//stud.Student.getAll();
+//Student.test();
 
 //stud.getAll();
+test();
+
