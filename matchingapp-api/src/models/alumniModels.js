123b82bd81f8ae00c54e45579c09e2be4bfc8b3f
sshirsath3 <sshirsath3@gatech.edu>
2021-02-26 15:38:39 -0500

    promisified alumni models API

--- matchingapp-api/src/models/alumniModels.js
+++ matchingapp-api/src/models/alumniModels.js
@@ -1,4 +1,5 @@
-var connection = require('../dbInit');
+const connection = require('../dbInit');
+const bcrypt = require('bcrypt');
 //var connection = db.dbConn;
 
 const Alumni = function(alumni) {
@@ -13,65 +14,75 @@ const Alumni = function(alumni) {
 }
 
 Alumni.createAlumni = ( params ) => {
-    var { username, firstName, lastName, middleName, email, pwd, bio } = params;
-    var query = `INSERT INTO Alumni values (${username}, ${pwd}, ${email}, ${firstName}, ${lastName}, ${middleName}, ${bio})`;
-    connection.query(query, (error, result, fields) => {
-        if (error) {
-            console.error(error);
+    if (params.username && params.firstName && params.lastName && params.email && params.pwd && params.bio) {
+        let hash = '';
+        try {
+            hash = await bcrypt.hash(params.pwd, 10);
+        } catch (e) {
+            throw 'ERROR OCCURED';
         }
-        console.log(result);
-        console.log(`Fields: ${fields}`);
-        return result;
-    });
+        params.pwd = hash;
+        let query = `INSERT INTO Alumni SET ?`;
+        try {
+            var alumni = await connection.query(query, params);
+        } catch (e) {
+            throw e;
+        }
+        return alumni;
+    } else {
+        throw 'ERROR OCCURRED';
+    }
 }
 
 Alumni.updateAlumni = ( params ) => {
-    var { username, firstName, lastName, middleName, email, pwd, bio, alumniID } = params;
-    var query = `UPDATE alumni SET username = ${username}, pwd = ${pwd}, email = ${email}, firstName = ${firstName}, lastName = ${lastName}, middleName = ${middleName}, bio = ${bio} WHERE id = ${alumniID}`;
-    connection.query(query, (error, result, fields) => {
-        if (error) {
-            console.log(error);
+    const {username} = params;
+    const inputs = Object.assign({}, params);
+    delete inputs.username;
+    let query = `UPDATE alumni SET ? WHERE username = "${username}"`;
+    if (inputs.hasOwnProperty('pwd')) {
+        if (inputs.pwd != null && inputs.pwd != '') {
+            return bcrypt.hash(inputs.pwd, 10, (err, hash) => {
+                if (err) throw 'ERROR OCCURRED WHILE UPDATING ALUMNI';
+                inputs.pwd = hash;
+                let alumni = {};
+                try {
+                    alumni = await connection.query(query, inputs);
+                } catch (e) {
+                    throw e;
+                }
+                return alumni;
+            });
         }
-        console.log(result);
-        console.log(`Fields: ${fields}`);
-        return result;
-    });
+    } else {
+        let alumni = await connection.query(query, inputs);
+        return alumni;
+    }
 }
 
-Alumni.findAlumniByName = ( name ) => {
-    var query = `SELECT username, email, firstName, lastName, middleName, bio FROM Alumni WHERE firstName like %${name}% or lastName like %${name}% or middleName like %${name}%`;
-    connection.query(query, (error, result, fields) => {
-        if (error) {
-            console.error(error);
-        }
-        console.log(`Fields: ${fields}`);
-        console.log(result);
-        return result;
-    });
+Alumni.findAlumniByName = ( params ) => {
+    // name can be first/middle/last name or username
+    const {name} = params;
+    if (name) {
+        let query = `SELECT username, email, firstName, lastName, middleName, bio FROM Alumni WHERE firstName like %${name}% or lastName like %${name}% or middleName like %${name}% or username like %${name}%;`;
+    } else {
+        let query = `SELECT username, email, firstName, lastName, middleName, bio FROM Alumni`;
+    }
+    let alumni = await connection.query(query);
+    return alumni;
 };
 
-Alumni.getAll = ( name ) => {
-    var query = `SELECT username, email, firstName, lastName, middleName, bio FROM Alumni`;
-    connection.query(query, (error, result, fields) => {
-        if (error) {
-            console.error(error);
-        }
-        console.log(`Fields: ${fields}`);
-        console.log(result);
-        return result;
-    });
+Alumni.getAll = ( params ) => {
+    let query = `SELECT username, email, firstName, lastName, middleName, bio FROM Alumni`;
+    let alumni = await connection.query(query);
+    return alumni;
 };
 
-Alumni.deleteAlumniAccount = (alumni) => {
-    var query = `DELETE FROM Alumni WHERE id = ${alumni.alumniID}`;
-    connection.query(query, (error, result, fields) => {
-        if (error) {
-            console.error(error);
-        }
-        console.log(`Fields: ${fields}`);
-        console.log(result);
-        return result;
-    });
+Alumni.deleteAlumniAccount = ( params ) => {
+    // deletes via the Alumni username NOT id
+    const {username} = params;
+    let query = `DELETE FROM Alumni WHERE username = ${username}`;
+    let alumni = await connection.query(query);
+    return alumni;
 };
 
 module.exports = Alumni
