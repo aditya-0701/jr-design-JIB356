9b9e86888c4f8f5941982af312f547b56dc59b79
Hye Lim Kim <hkim946@gatech.edu>
2021-02-26 07:21:31 -0500

    Updating project view

--- jr_design_demo/src/screens/ViewProject/index.js
+++ jr_design_demo/src/screens/ViewProject/index.js
@@ -1,4 +1,5 @@
-import * as React from 'react';
+// import * as React from 'react';
+import React from 'react';
 import { View, ScrollView, Text, TextInput, StyleSheet, TouchableOpacity, Button, Image, KeyboardAvoidingView, Dimensions, Animated, PanResponder } from 'react-native';
 // import { Card, ListItem, Button, Icon } from 'react-native-elements'
 import Ionicons from 'react-native-vector-icons/Ionicons'
@@ -76,7 +77,7 @@ export class Card extends React.Component {
 
     this.position = new Animated.ValueXY()
     this.state = {
-      currentIndex: 0 //projectDetails.indexOf("id")
+      currentIndex: 0
     }
     this.rotate = this.position.x.interpolate({
       inputRange: [-SCREEN_WIDTH / 2, 0, SCREEN_WIDTH / 2],
@@ -91,18 +92,6 @@ export class Card extends React.Component {
       ...this.position.getTranslateTransform()
       ]
     }
-
-    this.likeOpacity = this.position.x.interpolate({
-      inputRange: [-SCREEN_WIDTH / 2, 0, SCREEN_WIDTH / 2],
-      outputRange: [0, 0, 1],
-      extrapolate: 'clamp'
-    })
-
-    this.dislikeOpacity = this.position.x.interpolate({
-      inputRange: [-SCREEN_WIDTH / 2, 0, SCREEN_WIDTH / 2],
-      outputRange: [1, 0, 0],
-      extrapolate: 'clamp'
-    })
 
     this.nextCardOpacity = this.position.x.interpolate({
       inputRange: [-SCREEN_WIDTH / 2, 0, SCREEN_WIDTH / 2],
@@ -121,14 +110,14 @@ export class Card extends React.Component {
 
       onStartShouldSetPanResponder: (evt, gestureState) => true,
       onPanResponderMove: (evt, gestureState) => {
-        this.useNativeDriver = false;
-        this.position.setValue({ x: gestureState.dx, y: gestureState.dy })
+        this.useNativeDriver = true,
+          this.position.setValue({ x: gestureState.dx, y: gestureState.dy })
       },
       onPanResponderRelease: (evt, gestureState) => {
         if (gestureState.dx > 120) {
           Animated.spring(this.position, {
-            toValue: { x: SCREEN_WIDTH + 100, y: gestureState.dy },
-            useNativeDriver: true
+            toValue: { x: SCREEN_WIDTH + 100, y: gestureState.dy }
+            , useNativeDriver: true
           }).start(() => {
             this.setState({ currentIndex: this.state.currentIndex + 1 }, () => {
               this.position.setValue({ x: 0, y: 0 })
@@ -148,8 +137,8 @@ export class Card extends React.Component {
         else {
           Animated.spring(this.position, {
             toValue: { x: 0, y: 0 },
-            friction: 4,
-            useNativeDriver: true
+            friction: 4
+            , useNativeDriver: true
           }).start()
         }
       }
@@ -167,17 +156,6 @@ export class Card extends React.Component {
           <Animated.View
             {...this.PanResponder.panHandlers}
             key={item.id} style={[this.rotateAndTranslate, { height: SCREEN_HEIGHT - 120, width: SCREEN_WIDTH, padding: 10, position: 'absolute' }]}>
-            <Animated.View style={{ opacity: this.likeOpacity, transform: [{ rotate: '-30deg' }], position: 'absolute', top: 50, left: 40, zINdex: 1000 }}>
-              <Text style={{ borderWidth: 1, borderColor: 'green', color: 'green', fontSize: 32, fontWeight: '800', padding: 10 }}>
-                LIKE
-              </Text>
-            </Animated.View>
-
-            <Animated.View style={{ opacity: this.dislikeOpacity, transform: [{ rotate: '30deg' }], position: 'absolute', top: 50, left: 40, zINdex: 1000 }}>
-              <Text style={{ borderWidth: 1, borderColor: 'red', color: 'red', fontSize: 32, fontWeight: '800', padding: 10 }}>
-                NOPE
-              </Text>
-            </Animated.View>
 
             <Image
               style={{ flex: 1, height: null, width: null, resizeMode: 'cover', borderRadius: 20 }}
@@ -211,9 +189,18 @@ export class Card extends React.Component {
 
         </View>
       </View>
-    )
+    );
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
 
 /* var i = 0;
 
